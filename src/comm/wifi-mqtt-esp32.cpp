18c4393e7b5b8d32c16d27f80773ded9504c16d6
gdevalois <gdv@devicenut.com>
2022-03-22 16:12:14 -0700

    Remove redundant message prefixes.

--- src/comm/wifi-mqtt-esp32.cpp
+++ src/comm/wifi-mqtt-esp32.cpp
@@ -126,11 +126,11 @@ void WiFiMqtt::ConnectOTA(void)
     {
       Serial.printf("OTA: Error[%u]: ", error);
 
-           if (error == OTA_AUTH_ERROR)     Serial.println("OTA: Auth Failed");
-      else if (error == OTA_BEGIN_ERROR)    Serial.println("OTA: Begin Failed");
-      else if (error == OTA_CONNECT_ERROR)  Serial.println("OTA: Connect Failed");
-      else if (error == OTA_RECEIVE_ERROR)  Serial.println("OTA: Receive Failed");
-      else if (error == OTA_END_ERROR)      Serial.println("OTA: End Failed");
+           if (error == OTA_AUTH_ERROR)     Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR)    Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR)  Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR)  Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR)      Serial.println("End Failed");
     });
 
   ArduinoOTA.begin();
